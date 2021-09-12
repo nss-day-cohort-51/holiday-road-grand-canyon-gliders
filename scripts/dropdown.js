4fcfea3557166de8dd029d4ea37fae46bd696b01
Brady Williams <williams.cole29@yahoo.com>
2021-09-12 15:15:56 -0500

    working again

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,1 +1,124 @@
-fqfwefewfew
+import { stateCodes } from "./tools/StateCodes.js";
+import { settings } from "./Settings.js";
+
+const key = settings.npsKey;
+
+//StateCode DropDown
+export const createStateDrop = () => {
+    let dropdown = document.getElementById('dropState');
+    dropdown.length = 0;
+
+    let defaultOption = document.createElement('option');
+    defaultOption.text = 'Choose A State';
+
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
+
+
+    for(let i = 0; i < stateCodes.length; i++){
+        let option = document.createElement('option');
+        option.text = stateCodes[i];
+        dropdown.add(option);
+    }
+}
+
+//Park DropDown
+export const createDropDownPark = (input) => {
+
+    let dropdown = document.getElementById('dropPark');
+    dropdown.length = 0;
+
+    let defaultOption = document.createElement('option');
+    defaultOption.text = 'Choose A Park';
+
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
+
+    const url = `https://developer.nps.gov/api/v1/parks?stateCode=${input}&api_key=${key}`;
+
+    fetch(url)
+        .then(
+            function (response) {
+                // Examine the text in the response  
+                response.json().then(function (dataVar) {
+                    let option;
+
+                     for (let i = 0; i < dataVar.data.length; i++) {
+                         option = document.createElement('option');
+                         option.text = dataVar.data[i].fullName;
+                         dropdown.add(option);
+                     }
+                });
+            }
+        )
+}
+
+export const createDropdownParkFrame = () => {
+    return `
+    <select name="dropPark" id="dropPark">
+                
+    </select>
+    `
+}
+
+//Eateries DropDown
+export const createDropDownEat = () => {
+
+    let dropdown = document.getElementById('dropEat');
+    dropdown.length = 0;
+
+    let defaultOption = document.createElement('option');
+    defaultOption.text = 'Choose A Eaterie';
+
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
+
+    const url = 'http://holidayroad.nss.team/eateries';
+
+    fetch(url)
+        .then(
+            function (response) {
+                // Examine the text in the response  
+                response.json().then(function (data) {
+                    let option;
+
+                    for (let i = 0; i < data.length; i++) {
+                        option = document.createElement('option');
+                        option.text = data[i].businessName;
+                        dropdown.add(option);
+                    }
+                });
+            }
+        )
+}
+
+//Bizarreries DropDown
+export const createDropDownBiz = () => {
+
+    let dropdown = document.getElementById('dropBiz');
+    dropdown.length = 0;
+
+    let defaultOption = document.createElement('option');
+    defaultOption.text = 'Choose A Bizarrerie';
+
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
+
+    const url = 'http://holidayroad.nss.team/bizarreries';
+
+    fetch(url)
+        .then(
+            function (response) {
+                // Examine the text in the response  
+                response.json().then(function (data) {
+                    let option;
+
+                    for (let i = 0; i < data.length; i++) {
+                        option = document.createElement('option');
+                        option.text = data[i].name;
+                        dropdown.add(option);
+                    }
+                });
+            }
+        )
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,42 @@
-import { getBizarreries } from "./bizarreries/BizarreriesDataManager.js";
-import { getEateries } from "./eateries/EateryDataManager.js";
-import { getParks } from "./parks/ParkDataManager.js";
+import { createStateDrop, createDropDownPark, createDropdownParkFrame, createDropDownBiz, createDropDownEat} from "./dropdown.js"
 
-getEateries().then(eatery => {
-    console.log(eatery);
+ //get element statements
+const contentElement = document.getElementById("container");
+const populateDropDown = document.getElementById("drop-list")
+
+//Dropdown event listener
+let state;
+let park;
+let eat;
+let biz;
+contentElement.addEventListener("change", event => {
+    switch (event.target.id){
+        case "dropState":
+            state = event.target.value;
+            console.log(state)
+            populateDropDown.innerHTML = createDropdownParkFrame();
+            createDropDownPark(state);
+            break;
+        case "dropPark":
+            park = event.target.value;
+            console.log(park);
+            break;
+        case "dropEat":
+            eat = event.target.value;
+            console.log(eat);
+            break;
+        case "dropBiz":
+            biz = event.target.value;
+            console.log(biz);
+            break;
+    }
 })
 
-getBizarreries().then(bizarries => {
-    console.log(bizarries);
-})
 
-getParks("TX").then(parks => {
-    console.log(parks.data);
-});
+const runProgram = () => {
+    createDropDownBiz();
+    createDropDownEat();
+    createStateDrop();
+}
+
+runProgram();

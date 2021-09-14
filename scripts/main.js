a3ee96b4e132b7960c76b5bf83dfe57000e9333e
Brady Williams <williams.cole29@yahoo.com>
2021-09-14 18:28:34 -0500

    Added initial setup for directions to park from current location

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,59 @@ import { resetTripSelection, runDropdown } from "./dropdown.js";
 import { runModal } from "./modal.js";
 import { putTripCall } from "./data/DataManager.js";
 import { updateSavedTrips } from "./SavedTrips.js";
+import { getParkById } from "./parks/ParkDataManager.js";
+import { directionLiteral, getDirections } from "./directions/DirectionDataManager.js";
 
 runDropdown();
 runModal();
 // populate Saved Trips
 updateSavedTrips();
+
+const directionElement = document.querySelector(".directions-btn");
+const fillDirections = document.querySelector(".directions-fill");
+fillDirections.style.display = "block";
+
+//used for current location
+let currentLat;
+let currentLong;
+
+//Lines 22-29 used for gaining permission for location
+(function () {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        currentLat = position.coords.latitude;
+        currentLong = position.coords.longitude;
+    },
+        function (error) {
+            directionElement.style.display = "none";
+        })
+})();
+
+//used to gather the direction material through the parkid and current longitude and latitude
+directionElement.addEventListener("click", event => {
+
+    fillDirections.innerHTML = "";
+    if (
+        activeTripState.parkId != null &&
+        activeTripState.bazararieIds.length != 0 &&
+        activeTripState.eateryIds.length != 0
+    ) {
+        console.log(activeTripState.parkId);
+        getParkById(activeTripState.parkId).then(parkLoc => {
+            
+            const useVar = getDirections(currentLat, currentLong, parkLoc.latitude, parkLoc.longitude).then(function (event) {
+                console.log(event.paths[0].instructions.length);
+
+                for (let count = 0; count < event.paths[0].instructions.length; count++) {
+
+                    fillDirections.innerHTML += directionLiteral(event.paths[0].instructions[count].text);
+
+                }
+            })
+        });
+
+    }
+
+})
 
 // saveActive trip while still being filled out
 let activeTripState = {
--- scripts/parks/ParkDataManager.js
+++ scripts/parks/ParkDataManager.js
@@ -4,14 +4,14 @@ import { getWeatherZip } from "../weather/WeatherDataManager.js";
 
 export const getParks = (input) => {
 
-const key = settings.npsKey;
-const url = `https://developer.nps.gov/api/v1/parks?stateCode=${input}&api_key=${key}`;
+    const key = settings.npsKey;
+    const url = `https://developer.nps.gov/api/v1/parks?stateCode=${input}&api_key=${key}`;
 
-return fetch(url)
-    .then(response => response.json())
-    .then(parsedResponse => {
-        return parsedResponse;
-    })
+    return fetch(url)
+        .then(response => response.json())
+        .then(parsedResponse => {
+            return parsedResponse;
+        })
 
 }
 
@@ -21,30 +21,43 @@ export const getZip = (input) => {
 
     const key = settings.npsKey;
     const url = `https://developer.nps.gov/api/v1/parks?parkCode=${input}&api_key=${key}`;
-    
+
     return fetch(url)
         .then(response => response.json())
         .then(parsedResponse => {
             return parsedResponse.data[0].addresses[0].postalCode;
         })
-    
-    }
+
+}
 
 //Used to get the parks full name by the parkCode
 export const getParkByCode = (input) => {
 
     const key = settings.npsKey;
     const url = `https://developer.nps.gov/api/v1/parks?parkCode=${input}&api_key=${key}`;
-    
+
     return fetch(url)
         .then(response => response.json())
         .then(parsedResponse => {
             return parsedResponse.data[0];
         })
-    
-    }
 
-    //Used as function for onParkChanged on main
+}
+
+export const getParkById = (input) => {
+
+    const key = settings.npsKey;
+    const url = `https://developer.nps.gov/api/v1/parks?id=${input}&api_key=${key}`;
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(parsedResponse => {
+            return parsedResponse.data[0];
+        })
+
+}
+
+//Used as function for onParkChanged on main
 export const onParkChanged = (parkId) => {
     const parkNamePreview = document.getElementById("parkPreview");
 

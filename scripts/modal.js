d8144aa5b88cbb71ae72d1a09f0f769906f67d74
Susie Stanley <susiestanley00@gmail.com>
2021-09-14 14:09:09 -0500

    updated modal.js

--- scripts/modal.js
+++ scripts/modal.js
@@ -31,7 +31,7 @@ const modalDetailsInsert = (modalId) => {
         <h2>${modalId} NAME GOES HERE</h2>
         <p>All the info about the ${modalId} the user chose from the dropdown menu goes here. If you click on the X or anywhere that's not on this popup, it will close this window.</p>
       </div> <!-- closes modal-content -->
-      `;
+      `
 };
 
 export const runModal = () => {
@@ -41,7 +41,7 @@ export const runModal = () => {
 
     document.addEventListener("click", (event) => {
         if (event.target.id.startsWith("detail-button")) {
-            console.log("clicked");
+            console.log("detail button clicked");
 
             const buttonId = event.target.id.split("--")[1];
             // modal display for the given feature
@@ -49,48 +49,8 @@ export const runModal = () => {
             modal.style.display = "block";
         }
         if (event.target.id.startsWith("close-modal")) {
+            console.log("modal X clicked");
             modal.style.display = "none";
         }
     });
-
-    // When the user clicks the button, open the modal
-
-    // parkBtn.onclick = function () {
-    //     modal.style.display = "block";
-    // };
-
-    // eatBtn.onclick = function () {
-    //     eatModal.style.display = "block";
-    // };
-
-    // bizBtn.onclick = function () {
-    //     bizModal.style.display = "block";
-    // };
-
-    // // When the user clicks on <span> (x), close the modal
-    // spanPark.onclick = function () {
-    //     modal.style.display = "none";
-    // };
-
-    // spanEats.onclick = function () {
-    //     eatModal.style.display = "none";
-    // };
-
-    // spanBiz.onclick = function () {
-    //     bizModal.style.display = "none";
-    // };
-
-    // // When the user clicks anywhere outside of the modal, close it
-    // window.onclick = function (event) {
-    //     switch (event.target.id) {
-    //         case modal:
-    //             modal.style.display = "none";
-    //         // case eatModal:
-    //         //     eatModal.style.display = "none";
-    //         // case bizModal:
-    //         //     bizModal.style.display = "none";
-    //     }
-    // };
-
-    // ************* END THE  MODAL JS *******************
-};
+}

aa68ac243a2b29f50592a9478c89cd50860bee52
Susie Stanley <susiestanley00@gmail.com>
2021-09-16 10:16:14 -0500

    fixed saved-trip typo

--- scripts/cards/SavedTrip.js
+++ scripts/cards/SavedTrip.js
@@ -39,7 +39,7 @@ export const savedTripCardDetails = (tripDetails, directionId) => {
         card += `</div>`;
     }
 
-    card += `<button id="directions-btn--${directionId} "class="directions-btn">DIRECTIONS</button>
+    card += `<button id="directions-btn--${directionId}" class="directions-btn">DIRECTIONS</button>
     </div>`;
     return card;
 };
